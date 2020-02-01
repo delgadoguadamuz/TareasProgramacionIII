e746bccca4224d6409450608b74b0ee2e3e361ba
OSCAR DELGADO GUADAMUZ <oscar.delgado1@uamcr.net>
2020-01-31 23:10:17 -0600

    commit

---
+++ Tarea1/Tarea1/Interface/Reproductores.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1.Interface
+{
+    public class Reproductores 
+    {
+
+        #region Atributos
+        private string _play;
+        private string _stop;
+        private string _fw;
+        private string _rw;
+
+       
+        #endregion
+
+        #region Propiedades
+        public string Play1 { get => _play; set => _play = value; }
+        public string Stop1 { get => _stop; set => _stop = value; }
+        public string Fw { get => _fw; set => _fw = value; }
+        public string Rw { get => _rw; set => _rw = value; }
+        #endregion
+
+
+        #region Constructores
+               
+        public void Play(String play)
+        {
+            _play = play;
+            Console.WriteLine("Ejecutando"+ play);
+        }
+
+        public void Stop(String stop)
+        {
+            _stop = stop;
+            Console.WriteLine("Ejecutando" + stop);
+
+        }
+
+        public void FW(String fw)
+        {
+            _fw = fw;
+            Console.WriteLine("Ejecutando" + fw);
+        }
+
+
+        public void RW(string rw)
+        {
+            _rw = rw;
+            Console.WriteLine("Ejecutando" + rw);
+
+        }
+        #endregion
+
+    }
+}
---
+++ Tarea1/Tarea1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea1.Interface;
+
+namespace Tarea1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reproductores reproductores = new Reproductores();
+
+            reproductores.Play();
+            Console.ReadKey();
+
+        }
+    }
+}
